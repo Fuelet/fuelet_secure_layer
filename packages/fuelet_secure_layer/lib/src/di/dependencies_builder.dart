1f5088027fad67bb057e9a4bf11d0ceaa306791e
mpoplavkov <mihail.poplavkov@gmail.com>
2024-09-30 16:30:36 +0100

    Use a different password for session storage

--- packages/fuelet_secure_layer/lib/src/di/dependencies_builder.dart
+++ packages/fuelet_secure_layer/lib/src/di/dependencies_builder.dart
@@ -2,10 +2,10 @@ import 'package:fuelet_secure_layer/src/di/common/common_register.dart';
 import 'package:fuelet_secure_layer/src/di/public/public_register.dart';
 
 class SecureLayerDependenciesBuilder {
-  static Future<void> build(String cloudBackupAesPassword) async {
+  static Future<void> build(
+      String cloudBackupAesPassword, String sessionsStorageAesPassword) async {
     await CommonSecureLayerRegister.init();
-    await PrivateSecureLayerRegister.init(
-        cloudBackupAesPassword); // TODO use another pwd
+    await PrivateSecureLayerRegister.init(sessionsStorageAesPassword);
     await PublicSecureLayerRegister.init(cloudBackupAesPassword);
   }
 }
